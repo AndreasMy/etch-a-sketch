a2a56904f22ebe99699cd1e1f724237f401c3a18
AndreasMy <amykle85@gmail.com>
2023-02-11 21:19:38 +0100

    reset canvas, random color and black color have their respecitve buttons

--- script/script.js
+++ script/script.js
@@ -19,13 +19,45 @@ function calcNumberOfItems(x) {
   }
 }
 
-calcNumberOfItems(16);
+//todo implement a slider that effects a variable with value 0 - 100
+//todo pass that variable as an argument and assign function to button
+calcNumberOfItems(64);
+
+function reset() {
+  let squares = document.querySelectorAll(".grid-item");
+
+  for (let square of squares) {
+    square.style.backgroundColor = "white";
+  }
+}
 
 //? querySelectorAll works well with for...of
-let squares = document.querySelectorAll(".grid-item");
+function colorBlack() {
+  let squares = document.querySelectorAll(".grid-item");
 
-for (let square of squares) {
-  square.addEventListener("mouseover", function () {
-    square.style.backgroundColor = "black";
-  });
+  for (let square of squares) {
+    square.addEventListener("mouseover", function () {
+      square.style.backgroundColor = "black";
+    });
+  }
+}
+
+colorBlack();
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 256);
+}
+
+function getRandomRgb() {
+  let colorSquares = document.querySelectorAll(".grid-item");
+
+  for (let colorSquare of colorSquares) {
+    let r = getRandomNum();
+    let g = getRandomNum();
+    let b = getRandomNum();
+
+    colorSquare.addEventListener("mouseover", function () {
+      colorSquare.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
+  }
 }
