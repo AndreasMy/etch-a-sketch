1bc10f0ae11589b65bf75fbf5f943cde6ac31233
AndreasMy <amykle85@gmail.com>
2023-02-11 16:42:28 +0100

    grid adapts to value gained from prompt

--- script/script.js
+++ script/script.js
@@ -1,11 +1,27 @@
 //? find out how to loop a grid generation
 const getGridContainer = document.querySelector("#gridContainer");
-const gridSize = 1024;
+/* const gridSize = 16; */
 
 //? create rows and make an algorithm that generates the right amounts of rows
 //? alternatively, use flexbox to create a generate a 3x3 grid
 
-for (let i = 1; i <= gridSize; i++) {
-  getGridContainer.innerHTML += `<div class="grid-item"></div>`;
+/* for (let i = 1; i <= gridSize; i++) {
+  getGridContainer.innerHTML += `<div class="grid-item${[i]} grid-item"></div>`;
+} */
+
+function calcNumberOfItems() {
+  const x = parseInt(prompt(Number, 3));
+  const numberOfItems = x * x;
+  const rows = numberOfItems / x;
+
+  //? inline generation of css code using the rows value
+  getGridContainer.style = `  grid-template-columns: repeat(${rows}, 1fr);
+                              grid-template-rows: repeat(${rows}, 1fr);`
+
+  //? then loop the generation of items
+  for (let i = 1; i <= numberOfItems; i++ ) {
+    getGridContainer.innerHTML += `<div class="grid-item${[i]} grid-item"></div>`;
+  }
 }
 
+calcNumberOfItems()
