2cb4662757bc81830227a65531e051a51e569435
AndreasMy <amykle85@gmail.com>
2023-02-18 10:23:08 +0100

    draw functionality is now in the createGrid function and the drifferent drawing modes are tied to the value of a single global variable

--- script/script.js
+++ script/script.js
@@ -13,77 +13,30 @@ colorRainbowBtn = document.querySelector("#colorRainbowBtn");
 
 colorResetBtn.addEventListener("click", reset);
 
+//? create global variable
+let coloringMode = "";
+
 //? Draw Black
 colorBlackBtn.addEventListener("click", function () {
-  drawBlack();
+  coloringMode = "black";
+  console.log(coloringMode);
+  return coloringMode;
 });
 
 //? Draw Shading
 colorShadingBtn.addEventListener("click", function () {
-  drawShade();
+  coloringMode = "shade";
+  console.log(coloringMode);
+  return coloringMode;
 });
 
 //? Draw rainbow
 colorRainbowBtn.addEventListener("click", function () {
-  drawRandomRgb();
+  coloringMode = "rainbow";
+  console.log(coloringMode);
+  return coloringMode;
 });
 
-function drawBlack() {
-  const blackGridContainer = document.querySelector("#gridContainer");
-  for (let i = 0; i < blackGridContainer.children.length; i++) {
-    blackGridContainer.children[i].addEventListener("mouseover", function () {
-      this.style.backgroundColor = `red`;
-    });
-    blackGridContainer.children[i].addEventListener("mouseleave", function () {
-      this.style.backgroundColor = `black`;
-    });
-  }
-}
-
-
-function drawRandomRgb() {
-  const rgbGridContainer = document.querySelector("#gridContainer");
-  for (let i = 0; i < getGridContainer.children.length; i++) {
-    rgbGridContainer.children[i].addEventListener("mouseover", function () {
-      this.style.backgroundColor = `red`;
-    });
-    rgbGridContainer.children[i].addEventListener("mouseleave", function () {
-      let randomColor = `rgb(${Math.floor(Math.random() * 255)}, 
-      ${Math.floor(Math.random() * 255)}, 
-      ${Math.floor(Math.random() * 255)})`;
-      this.style.backgroundColor = randomColor;
-    });
-  }
-}
-let newRgb;
-
-function drawShade() {
-  const shadeGridContainer = document.querySelector("#gridContainer");
-  
-  for (let i = 0; i < shadeGridContainer.children.length; i++) {
-    shadeGridContainer.children[i].addEventListener("mouseover", function () {
-      let cssObj = window.getComputedStyle(this, null);
-      let bgColor = cssObj.getPropertyValue("background-color");
-      let numFromString = bgColor.match(/\d+/g).map(Number);
-      newRgb = numFromString.map((value) => (value * 0.9).toFixed(0));
-      
-      rgbValue.innerHTML = `${numFromString}`;
-      this.style.backgroundColor = `red`;
-    });
-    shadeGridContainer.children[i].addEventListener("mouseleave", function () {
-      this.style.backgroundColor = `rgb(${newRgb})`;
-    });
-  }
-}
-
-/* If you want practice with removeEventListener, that's fine. 
-Personally, I would suggest attaching one listener when the cells are created.
- And in that listener, perhaps, check a global coloringMode variable to see which 
- way to color the cell.
-
-The buttons would just change the value of that coloringMode, and the event listener 
-would dynamically call one function or the other, depending on that coloringMode value.
- */
 function createGrid(amount) {
   let numberOfItems = amount * amount;
   const rows = numberOfItems / amount;
@@ -99,7 +52,30 @@ function createGrid(amount) {
     gridItem.className = `grid-item`;
     getGridContainer.appendChild(gridItem);
   }
+
+  for (let i = 0; i < getGridContainer.children.length; i++) {
+    getGridContainer.children[i].addEventListener("mouseover", function () {
+
+      if (coloringMode === "black") {
+        this.style.backgroundColor = `black`;
+
+      } else if (coloringMode === "shade") {
+        let cssObj = window.getComputedStyle(this, null);
+        let bgColor = cssObj.getPropertyValue("background-color");
+        let numFromString = bgColor.match(/\d+/g).map(Number);
+        newRgb = numFromString.map((value) => (value * 0.9).toFixed(0));
+        this.style.backgroundColor = `rgb(${newRgb})`;
+
+      } else if (coloringMode === "rainbow") {
+        let randomColor = `rgb(${Math.floor(Math.random() * 255)}, 
+        ${Math.floor(Math.random() * 255)}, 
+        ${Math.floor(Math.random() * 255)})`;
+        this.style.backgroundColor = randomColor;
+      }
+    });
+  }
 }
+
 createGrid(16);
 
 //? function to remove each element
@@ -126,3 +102,13 @@ slider.onmouseup = function () {
   rmElement();
   createGrid(slider.value);
 };
+
+//! help received on discord: 
+/* If you want practice with removeEventListener, that's fine. 
+ Personally, I would suggest attaching one listener when the cells are created.
+ And in that listener, perhaps, check a global coloringMode variable to see which 
+ way to color the cell.
+
+The buttons would just change the value of that coloringMode, and the event listener 
+would dynamically call one function or the other, depending on that coloringMode value.
+ */
