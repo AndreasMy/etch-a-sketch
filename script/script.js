4a8ba69ed4ed85486d1b5af9b0bf6a0ecdc939c6
AndreasMy <amykle85@gmail.com>
2023-02-19 18:58:42 +0100

    added eraser and toggle grid button

--- script/script.js
+++ script/script.js
@@ -6,35 +6,51 @@ let sliderOutput = document.querySelector("#sliderValue");
 sliderOutput.innerHTML = `${slider.value} x ${slider.value}`;
 
 //? buttons:
-colorResetBtn = document.querySelector("#colorResetBtn");
-colorBlackBtn = document.querySelector("#colorBlackBtn");
-colorShadingBtn = document.querySelector("#colorShadingBtn");
-colorRainbowBtn = document.querySelector("#colorRainbowBtn");
+const colorResetBtn = document.querySelector("#colorResetBtn");
+const colorBlackBtn = document.querySelector("#colorBlackBtn");
+const colorShadingBtn = document.querySelector("#colorShadingBtn");
+const colorRainbowBtn = document.querySelector("#colorRainbowBtn");
+const colorEraserBtn = document.querySelector("#colorEraserBtn");
+const toggleGrid = document.querySelector("#toggleGrid");
+
+//! create global variable
+let coloringMode = "";
+let toggle = "grid on";
 
 colorResetBtn.addEventListener("click", reset);
-
-//? create global variable
-let coloringMode = "";
 
 //? Draw Black
 colorBlackBtn.addEventListener("click", function () {
   coloringMode = "black";
-  console.log(coloringMode);
   return coloringMode;
 });
 
 //? Draw Shading
 colorShadingBtn.addEventListener("click", function () {
   coloringMode = "shade";
-  console.log(coloringMode);
   return coloringMode;
 });
 
 //? Draw rainbow
 colorRainbowBtn.addEventListener("click", function () {
   coloringMode = "rainbow";
-  console.log(coloringMode);
   return coloringMode;
+});
+
+colorEraserBtn.addEventListener("click", function () {
+  coloringMode = "eraser";
+});
+
+toggleGrid.addEventListener("click", function () {
+  if (toggle === "grid on") {
+    toggle = "grid off";
+    setGridToggle()
+  } else if (toggle === "grid off") {
+    toggle = "grid on";
+    setGridToggle()
+  }
+  console.log(toggle)
+  return toggle;
 });
 
 function createGrid(amount) {
@@ -55,28 +71,42 @@ function createGrid(amount) {
 
   for (let i = 0; i < getGridContainer.children.length; i++) {
     getGridContainer.children[i].addEventListener("mouseover", function () {
-
       if (coloringMode === "black") {
         this.style.backgroundColor = `black`;
-
       } else if (coloringMode === "shade") {
         let cssObj = window.getComputedStyle(this, null);
         let bgColor = cssObj.getPropertyValue("background-color");
         let numFromString = bgColor.match(/\d+/g).map(Number);
         newRgb = numFromString.map((value) => (value * 0.9).toFixed(0));
-        this.style.backgroundColor = `rgb(${newRgb})`;
 
+        this.style.backgroundColor = `rgb(${newRgb})`;
       } else if (coloringMode === "rainbow") {
         let randomColor = `rgb(${Math.floor(Math.random() * 255)}, 
         ${Math.floor(Math.random() * 255)}, 
         ${Math.floor(Math.random() * 255)})`;
+
         this.style.backgroundColor = randomColor;
+      } else if (coloringMode === "eraser") {
+        this.style.backgroundColor = "white";
       }
+
+
     });
   }
 }
 
 createGrid(16);
+
+function setGridToggle() {
+  let squares = document.querySelectorAll(".grid-item");
+  for (let square of squares)
+  if (toggle === "grid on") {
+    square.style.border = "solid rgb(224, 224, 224) 1px"
+  } else if (toggle === "grid off") {
+    square.style.border = "solid rgb(224, 224, 224) 0"
+  }
+  console.log(toggle)
+}
 
 //? function to remove each element
 function rmElement() {
@@ -100,10 +130,17 @@ slider.oninput = function () {
 
 slider.onmouseup = function () {
   rmElement();
+  toggle = "grid on";
   createGrid(slider.value);
 };
 
-//! help received on discord: 
+
+//TODO customize slider color
+//TODO make it pretty
+//TODO make an invert button/brush
+//? is it possible to increase brush radius to trigger neighboring divs
+
+//! help received on discord:
 /* If you want practice with removeEventListener, that's fine. 
  Personally, I would suggest attaching one listener when the cells are created.
  And in that listener, perhaps, check a global coloringMode variable to see which 
